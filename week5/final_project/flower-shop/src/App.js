998bd1c000a395de2104eff61220c3292e4975dd
WEBDEVLOVE20 <kelly.compton@bryanuniversity.edu>
2021-08-07 21:41:17 -0400

    #

--- week5/final_project/flower-shop/src/App.js
+++ week5/final_project/flower-shop/src/App.js
@@ -1,11 +1,21 @@
+import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 
-function App() {
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>The react app</h1>
-      </header>
+    <div >
+      <Navbar />
+
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/login' component={Login} />
+      </Switch>
     </div>
   );
 }
---
+++ week5/final_project/flower-shop/src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './home.css';
+const Home = () => {
+  return (
+    <div>
+      <div className="title-container">
+        <h1>Best in Bloom</h1>
+        <h3>Flowers and Gifts</h3>
+      </div>
+  </div>
+  );
+}
+
+export default Home;
